429356494ef0cfc12e272d7e3e4404174938eeda
Юрий Мусихин <vensop@gmail.com>
2022-12-13 21:46:56 +0300

    update dz1

--- dz1/z1.py
+++ dz1/z1.py
@@ -8,7 +8,7 @@
 a = int(input("Введите день недели: "))
 if a<=5 and a>=1:
     print(f"{a} -> нет")
-elif a>=6 and a<=7:
+elif a==6 or a==7:
     print(f"{a} -> да")
 elif a>7 or a<1:
     print(f"{a} -> не правильный день недели")
--- dz1/z5.py
+++ dz1/z5.py
@@ -4,14 +4,14 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 import math
-def coord():
+def coord_input():
     x = float(input())
     y = float(input())
     return x,y
 
 
-x1,y1 = coord()
-x2,y2 = coord()
+x1,y1 = coord_input()
+x2,y2 = coord_input()
 
 d = math.sqrt(pow(x2-x1, 2) + pow(y2-y1, 2))
 
